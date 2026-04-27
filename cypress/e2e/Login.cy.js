68dea4871a44bb74cf2386d4b5ce03fd043130c3
Lojain Alomari <alomarilojain@gmail.com>
2026-04-27 13:40:00 +0300

    cy.get commit

--- cypress/e2e/Login.cy.js
+++ cypress/e2e/Login.cy.js
@@ -33,7 +33,7 @@ describe('Login', () => {
         // Arra nge
 
         // Acting
-        cy.get('[data-test="username"]').type('standard_user');
+        // cy.get('[data-test="username"]').type('standard_user');
         cy.get('[data-test="password"]').type('secret_sauce');
         cy.get('[data-test="login-button"]').click();
         cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click();
