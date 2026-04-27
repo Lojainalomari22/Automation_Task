7b6981bc7af4257656629f5e29f1267e05d20fc5
Lojain Alomari <alomarilojain@gmail.com>
2026-04-27 12:26:38 +0300

    edit TC

--- cypress/e2e/Login.cy.js
+++ cypress/e2e/Login.cy.js
@@ -42,7 +42,19 @@ describe('Login', () => {
         // Assert
         cy.get('[data-test="title"]').should('contain', 'Your Cart');
     });
+    it.only('Assert Checkout successfully', () => {
+        // Arra nge
 
+        // Acting
+        cy.get('[data-test="username"]').type('standard_user');
+        cy.get('[data-test="password"]').type('secret_sauce');
+        cy.get('[data-test="login-button"]').click();
+        cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+        cy.get('[data-test="shopping-cart-link"]').click();
+
+        // Assert
+        cy.get('[data-test="title"]').should('contain', 'Your Cart');
+    });
     it('Assert Order Created successfully', () => {
         cy.get('[data-test="username"]').type('standard_user');
         cy.get('[data-test="password"]').type('secret_sauce');
